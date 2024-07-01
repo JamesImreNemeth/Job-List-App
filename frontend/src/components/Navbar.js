ebb0665535857ebf8606d0db938f27fe2257b8cd
James Nemeth <jamesnemeth1999@gmail.com>
2024-07-01 11:38:25 +1000

    Init CSS added, setup pages and components folders

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <header>
+           <div className = 'container'>
+                <Link>
+                    <h1>Organise Your Job List fast and effectively.</h1>
+                </Link>
+           </div>
+            
+        </header>
+    )
+}
+
+export default Navbar
